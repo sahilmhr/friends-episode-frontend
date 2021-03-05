adbed99d71e44aa15ba910fa5100c2d7fb2e10c9
Sahil Mehra <sahilmehra2412@gmail.com>
2021-03-05 21:20:34 +0530

    Handled Null Check

--- script.js
+++ script.js
@@ -8,13 +8,22 @@ let loader = document.getElementById("loader");
 
 div.style.display = "none";
 
-let clickCounter =()=>{
-    fetch("https://api.countapi.xyz/hit/friends-site/click").then(response=>{
+let clickCounter = () => {
+    fetch("https://api.countapi.xyz/hit/friends-site/click").then(response => {
         return response.json();
-    }).then(data=>{
+    }).then(data => {
         console.log("Click")
         console.log(data);
     })
+}
+
+let validationCheck = (loader, input) => {
+    if (input.value.length == 0) {
+        input.classList.add("invalid");
+        loader.style.display = "none";
+    } else if (input.classList.contains("invalid")){
+        input.classList.remove("invalid");
+    }
 }
 
 let getEpisodes = () => {
@@ -22,6 +31,8 @@ let getEpisodes = () => {
     loader.style.display = "flex";
 
     tbody.innerHTML = "";
+
+    validationCheck(loader, input);
 
     let data = {
         "dialogue": input.value
@@ -34,57 +45,59 @@ let getEpisodes = () => {
         body: JSON.stringify(data)
     }
 
-    fetch(url, params).then(response => {
-        return response.json();
-    }).then(result => {
-        div.style.display = "block";
-        for (const entry of Object.entries(result)) {
-            let tr = document.createElement('tr');
-            let check = false;
-            for (let i = 0; i < 3; i++) {
-                let td = document.createElement("td");
-                td.classList.add("px-6", "py-4", "whitespace-nowrap");
-                let div1 = document.createElement("div");
-                div1.classList.add("flex", "items-center");
-                let div2 = document.createElement("div");
-                div2.classList.add("ml-4");
-                let div3 = document.createElement("div");
-                div3.classList.add("text-sm", "font-medium", "text-gray-100");
-                if (!isNaN(parseInt(entry[i]))) {
-                    div3.innerHTML = parseInt(entry[i]) + 1;
-                } else {
-                    let info = String(entry[1]);
-                    if (check) {
-                        div3.innerHTML = info;
+    if (input.value.length != 0) {
+        fetch(url, params).then(response => {
+            return response.json();
+        }).then(result => {
+            div.style.display = "block";
+            for (const entry of Object.entries(result)) {
+                let tr = document.createElement('tr');
+                let check = false;
+                for (let i = 0; i < 3; i++) {
+                    let td = document.createElement("td");
+                    td.classList.add("px-6", "py-4", "whitespace-nowrap");
+                    let div1 = document.createElement("div");
+                    div1.classList.add("flex", "items-center");
+                    let div2 = document.createElement("div");
+                    div2.classList.add("ml-4");
+                    let div3 = document.createElement("div");
+                    div3.classList.add("text-sm", "font-medium", "text-gray-100");
+                    if (!isNaN(parseInt(entry[i]))) {
+                        div3.innerHTML = parseInt(entry[i]) + 1;
                     } else {
-                        let season = parseInt(info.substring(7, 9));
-                        let episode = parseInt(info.substring(18));
-                        let patt = /\W\d{2}\W.*\d{2}/
-                        if (info.match(patt)) {
-                            fetch(`https://api.tvmaze.com/shows/431/episodebynumber?season=${season}&number=${episode}`).then(response => {
-                                if (response.ok) {
-                                    return response.json();
-                                }
-                                return "Some error!"
-                            }).then(data => {
-                                div3.innerHTML = data['name'];
-                            })
+                        let info = String(entry[1]);
+                        if (check) {
+                            div3.innerHTML = info;
                         } else {
-                            console.log("Regex")
+                            let season = parseInt(info.substring(7, 9));
+                            let episode = parseInt(info.substring(18));
+                            let patt = /\W\d{2}\W.*\d{2}/
+                            if (info.match(patt)) {
+                                fetch(`https://api.tvmaze.com/shows/431/episodebynumber?season=${season}&number=${episode}`).then(response => {
+                                    if (response.ok) {
+                                        return response.json();
+                                    }
+                                    return "Some error!"
+                                }).then(data => {
+                                    div3.innerHTML = data['name'];
+                                })
+                            } else {
+                                console.log("Regex")
+                            }
+                            check = true;
                         }
-                        check = true;
                     }
+                    div2.append(div3);
+                    div1.append(div2);
+                    td.append(div1);
+                    tr.append(td);
                 }
-                div2.append(div3);
-                div1.append(div2);
-                td.append(div1);
-                tr.append(td);
-            }
-            tbody.append(tr);
-        };
-        loader.style.display = "none";
-    })
-    clickCounter();
+                tbody.append(tr);
+            };
+            loader.style.display = "none";
+        })
+        clickCounter();
+    }
 }
 
 btn.addEventListener('click', getEpisodes);
